820aa630e089111a97484408c93e32b453177bb9
opariffazman <opariffazman@gmail.com>
2021-09-30 16:00:15 +0800

    Added COLORREF for Boons

--- ChromaImplementation.h
+++ ChromaImplementation.h
@@ -34,6 +34,16 @@ const COLORREF CYAN = RGB(00, 255, 255);
 const COLORREF ORANGE = RGB(255, 165, 00);
 const COLORREF PINK = RGB(255, 192, 203);
 const COLORREF GREY = RGB(125, 125, 125);
+
+const COLORREF ZEUS = RGB(235, 206, 87);
+const COLORREF ARES = RGB(255, 94, 73);
+const COLORREF ARTEMIS = RGB(210, 255, 97);
+const COLORREF APHRODITE = RGB(255, 196, 240);
+const COLORREF ATHENA = RGB(253, 189, 49);
+const COLORREF POSEIDON = RGB(139, 199, 253);
+const COLORREF DEMETER = RGB(253, 189, 49);
+const COLORREF HERMES = RGB(255, 120, 0);
+const COLORREF CHAOS = RGB(100, 25, 255);
 //You dont have to define your colors as COLORREFs, just use the RGB(xxx,xxx,xxx) function like above
 
 #define ALL_DEVICES         0
@@ -49,7 +59,9 @@ class ChromaImplementation
 public:
 	ChromaImplementation();
 	~ChromaImplementation();
+	//BOOL Initialize();
 	BOOL Initialize();
+	void UnInitialize();
 
 	void ResetEffects(size_t DeviceType);
 	//Define your methods here
@@ -57,12 +69,17 @@ public:
 	void SetEffectImpl(RZEFFECTID EffectId);
 	void CreateEffectGroup(RZEFFECTID* pGroupEffectId, BOOL Repeat = FALSE);
 	void AddToGroup(RZEFFECTID GroupEffectId, RZEFFECTID EffectId, LONG DelayMS = 100);
+	void DeleteEffectImpl(RZEFFECTID EffectId);
+	void StopEffectImpl(RZEFFECTID EffectId);
 
 	BOOL IsDeviceConnected(RZDEVICEID DeviceId);
 
 private:
 	HMODULE m_ChromaSDKModule;
-	
+	HANDLE m_ChromaSDKEvent;
+
 };
+
+extern ChromaImplementation g_ChromaImplementation;
 
 #endif
